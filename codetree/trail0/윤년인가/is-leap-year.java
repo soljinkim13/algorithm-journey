8fe5d458162e2fc88657b21d439fdede0a471d82
soljin_k <soljinkim13@gmail.com>
2026-07-22 01:47:10 +0900

    [Passed] 윤년인가 (124ms, 10MB)

---
+++ codetree/trail0/윤년인가/is-leap-year.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int Y = sc.nextInt();
+
+        if ((Y % 4 == 0 && Y % 100 != 0) || Y % 400 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
